0ab3dfc794458b6a9515a3aaaccebf69a7fe673f
ZeroWM <wm4968@gmail.com>
2022-12-08 15:13:06 +0800

    version1

--- src/main/java/bcsx/Chapter3Solution1.java
+++ src/main/java/bcsx/Chapter3Solution1.java
@@ -6,6 +6,7 @@ public class Chapter3Solution1 {
   public static void main(String[] args) {
     Random random = new Random();
     System.out.println(random.nextInt(10));
+    System.out.println("AAA");
   }
 }
 
